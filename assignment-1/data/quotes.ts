fae42b87cba08c6c2ab9db539060c978111200a1
ahmedawan71 <m.ahmedse05@gmail.com>
2025-07-03 09:06:49 +0500

    Second Day

---
+++ assignment-1/data/quotes.ts
@@ -0,0 +1,7 @@
+
+export const quotes = [
+  { id: 1, text: "Believe you can and you’re halfway there." },
+  { id: 2, text: "Your limitation—it’s only your imagination." },
+  { id: 3, text: "Push yourself, because no one else is going to do it for you." },
+ 
+];
---
+++ assignment-1/src/app/page.tsx
@@ -0,0 +1,11 @@
+// assignment-1/app/page.tsx
+import { QuoteList } from "./components/QuoteList";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold mb-6">Quote Generator</h1>
+      <QuoteList />
+    </main>
+  );
+}
